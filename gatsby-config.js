594d429aa4bff8acd5dfc2dc650b13e0af7cf241
chernandez7 <chris00hernandez@gmail.com>
2018-12-02 19:20:31 -0600

    update manifest name

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
+        name: "chrishernandez.io",
         short_name: "starter",
         start_url: "/",
         background_color: "#222",
