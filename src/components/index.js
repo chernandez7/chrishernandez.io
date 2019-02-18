8d512e4c96aae29a5935d6327aa6e738f950f698
chernandez7 <chris00hernandez@gmail.com>
2019-02-18 01:32:24 -0600

    update social icons

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Heading } from "./Heading";
 export { default as Layout } from "./layout";
 export { default as Image } from "./image";
 export { default as NotFound } from "./NotFound";
+export { default as SimpleIcon } from "./SimpleIcon";
